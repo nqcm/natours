332f6fe73c1ef173f91838e3e364943d9d97614b
TheNiqabiCoderMum <ndhedhi@gmail.com>
2020-12-30 22:00:56 +0200

    Fixed typo

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -49,15 +49,15 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
 const createBookingCheckout = async session => {
   const tour = session.client_reference_id
-  const user = await User.findOne({ email: session.customer_email }).id
+  const user = (await User.findOne({ email: session.customer_email })).id
   const price = session.display_items[0].amount / 100
   await Booking.create({ tour, user, price })
 }
 
 exports.webhookCheckout = (req, res, next) => {
   const signature = req.headers['stripe-signature']
-  let event
 
+  let event
   try {
     const event = stripe.webhooks.constructEvent(
       req.body,
